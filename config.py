9da9bffccc313449065251b94d179a02fab46bba
MattySplatties <141183670+MattySplatties@users.noreply.github.com>
2024-02-29 12:28:00 -0600

    Delete config.py

--- config.py
+++
@@ -1,3 +0,0 @@
-#  Add your API key
-yelp_api_key = "Bearer ZDmorcxyc354E-dc7dNhOHTOQrkW3g9sAQByOVXjy_qkMYH8pssjBJNAbZnQqtrhR6JAGJCJeL60CU7M5wI_G5XQ2gZRjHn-Au2tMXXNboR8N0xQnjGkEZE9PrvWZXYx"
-google_api_key = "AIzaSyAzbqo-xMs8x2-gSQ0OCOoQWfDxgi7TBvY"
