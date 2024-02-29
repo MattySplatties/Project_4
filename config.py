841ce5ab8b0e8616620bd39dc8eddff0ef830933
MattySplatties <matthew.channell85@gmail.com>
2024-02-29 12:23:16 -0600

    sorting files

---
+++ config.py
@@ -0,0 +1,3 @@
+#  Add your API key
+yelp_api_key = "Bearer ZDmorcxyc354E-dc7dNhOHTOQrkW3g9sAQByOVXjy_qkMYH8pssjBJNAbZnQqtrhR6JAGJCJeL60CU7M5wI_G5XQ2gZRjHn-Au2tMXXNboR8N0xQnjGkEZE9PrvWZXYx"
+google_api_key = "AIzaSyAzbqo-xMs8x2-gSQ0OCOoQWfDxgi7TBvY"
